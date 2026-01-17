403e2bed245f34914e62fef22d4ea0db410fa14f
Max <46341774+Max-Soze@users.noreply.github.com>
2026-01-17 14:15:52 -0500

    certificate attempt

---
+++ web/src/lib/mongo_connection.js
@@ -0,0 +1,26 @@
+
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const credentials = process.env.MONGO_CERT_PATH
+
+const client = new MongoClient('mongodb+srv://clussy.ua97stz.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&appName=Clussy', {
+  tlsCertificateKeyFile: credentials,
+  serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("testDB");
+    const collection = database.collection("testCol");
+    const docCount = await collection.countDocuments({});
+    console.log(docCount);
+    // perform actions using client
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+export { run }
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,4 +1,11 @@
+<script>
+  import { run } from '$lib/mongo_connection.js';
+</script>
+
+
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<button action={ run } class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click me</button>
 
 <style lang="postcss">
   @reference "tailwindcss";
