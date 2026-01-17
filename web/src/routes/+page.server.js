67692b19c7eb1bfb1b87b41b415cd4c3fb4cd372
Jack Olsen <jackolsen2004@icloud.com>
2026-01-17 16:51:10 -0500

    WIP[1] adding courses

--- web/src/routes/+page.server.js
+++ web/src/routes/+page.server.js
@@ -1,12 +1,36 @@
 import { client } from '$lib/mongo_cert.js';
 
+export const prerender = false;
+
 /** @type {import('./$types').PageLoad} */
 
 export async function load({ params }) {
     await client.connect();
     const database = client.db("testDB");
     database.runCursorCommand({ ping: 1 });
-	return {
+    return {
         success: true
-	};
+    };
 }
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+    addCourse: async ({ request }) => {
+        const data = await request.formData();
+        const courseCode = data.get('courseCode');
+        const courseName = data.get('courseName');
+
+        if (!courseCode || !courseName) {
+            return { success: false, error: 'Missing fields' };
+        }
+
+        // Add course to database 
+        const database = client.db("testDB");
+
+
+
+        return { success: true };
+    }
+};
+
+
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
   import client from "react-dom/client";
 
-  let { data } = $props();
+  let { data, actions } = $props();
+  let addCourseModalOpen = $state(false);
 </script>
 
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
@@ -18,7 +19,19 @@
   >Click me</button
 >
 
-<button onClick={() => addCourse()}>Add Course</button>
+<button
+  onclick={() => (addCourseModalOpen = true)}
+  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >Add Course</button
+>
+
+{#if addCourseModalOpen === true}
+  <form method="POST" action="?/addCourse">
+    <input style="border: 1px solid black;" name="courseCode" />
+    <input style="border: 1px solid black;" name="courseName" />
+    <button type="submit">Add</button>
+  </form>
+{/if}
 
 <style lang="postcss">
   @reference "tailwindcss";
